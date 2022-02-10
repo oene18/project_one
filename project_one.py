a095faaa177027a3ef0cc3bddb102fe58208252d
Oene Stavenuiter <oene.stavenuiter@gmail.com>
2022-02-10 14:22:46 +0100

    "first commit"

---
+++ project_one.py
@@ -0,0 +1,16 @@
+"""blbla"""
+import sys
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who):
+    """greeting"""
+    greeting = f"Hello, {who}"
+    return greeting
+
+
+r = requests.get('https://coreyms.com')
+print(r.status_code)
